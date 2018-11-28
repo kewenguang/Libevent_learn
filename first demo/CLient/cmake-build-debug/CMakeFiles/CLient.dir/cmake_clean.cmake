37ec8ab8c242bf0f8674e5cffc52ae84c2e8fe87
kewenguang <2860881669@qq.com>
2018-11-28 20:49:21 +0800

    为了初始化库

---
+++ first demo/CLient/cmake-build-debug/CMakeFiles/CLient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLient.dir/main.cpp.o"
+  "CLient.pdb"
+  "CLient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
